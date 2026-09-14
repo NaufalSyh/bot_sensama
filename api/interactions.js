637848ac4eac5d3101858beb5475c4d33d582970
Muhammad Naufal Ardiansyah <143949563+NaufalSyh@users.noreply.github.com>
2026-09-14 22:48:40 +0700

    Updateinteractions

--- api/interactions.js
+++ api/interactions.js
@@ -29,6 +29,13 @@ export const config = {
 const PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
 
 export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).json({
+      success: true,
+      message: "Discord interaction endpoint aktif."
+    });
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false,
